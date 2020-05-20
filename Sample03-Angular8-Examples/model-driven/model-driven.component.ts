c22d3d7521ed23302f0544075e20bbac8516e23f
Shweta Tiwari <stiwari@chambal.com>
2020-05-20 11:47:15 +0530

    Simple validations, Nested Controls

--- Sample03-Angular8-Examples/model-driven/model-driven.component.ts
+++ Sample03-Angular8-Examples/model-driven/model-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+//import { FormGroup, FormControl } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-model-driven',
@@ -8,15 +9,64 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class ModelDrivenComponent implements OnInit {
 
-  constructor() { }
+  constructor( private fb: FormBuilder ) { }
 
-  registrationForm = new FormGroup({
-    userName : new FormControl('john'),
-    password : new FormControl(''),
-    confirmPassword: new FormControl('')
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  registrationForm = this.fb.group({
+    userName : ['', [Validators.required, Validators.minLength(3)]],
+    password: [''],
+    confirmPassword: [''],
+    address : this.fb.group({
+      city : [''],
+      state : [''],
+      pinCode : ['']
+    })
   });
 
   ngOnInit() {
+
   }
+
+  // Using FormGroup Classes
+  /*registrationForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword : new FormControl(''),
+    address : new FormGroup({
+      city : new FormControl(''),
+      state : new FormControl(''),
+      pinCode : new FormControl('')
+    })
+  });*/
+
+  // using setValue method
+  /*loadApiData(){
+    this.registrationForm.setValue({
+      userName: 'John Smith',
+      password: 'abcd',
+      confirmPassword: 'abcd',
+      address : {
+        city : 'Udaipur',
+        state: 'Raj',
+        pinCode : '313001'
+      }
+    })
+  }*/
+
+  // Using patchValue Method
+  loadApiData(){
+    this.registrationForm.patchValue({
+      password: 'abcd',
+      confirmPassword: 'abcd',
+      address : {
+        state: 'Raj',
+        pinCode : '313001'
+      }
+    })
+  }
+
 
 }
